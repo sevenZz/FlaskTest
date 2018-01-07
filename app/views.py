646242ab8c11e754ccea8f9dbbd76bde0526658c
zhangzhao <zhangzhao0501@sina.cn>
2018-01-07 22:14:40 +0800

    add some style to index

--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    print
+    return render_template('index.html')
